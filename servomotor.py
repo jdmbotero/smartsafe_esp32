634429254d6378968e99c31eba055c33bfdcbc98
July Ospina <jcobetancur@gmail.com>
2022-07-20 16:38:18 -0500

    general adjust in servo

--- servomotor.py
+++ servomotor.py
@@ -1,12 +1,21 @@
 from machine import Pin, PWM
+from utime import sleep_ms
 
 servo = PWM(Pin(15), freq=50)
 
 def open_door():
-    servo.duty(20)
+    i = 70
+    while i > 20:
+        i = i - 1
+        servo.duty(i)
+        sleep_ms(50)
 
 def close_door():
-    servo.duty(50)
+    i = 20
+    while i < 70:
+        i = i + 1
+        servo.duty(i)
+        sleep_ms(50)
     
 if __name__==("__main__"):
     close_door()
